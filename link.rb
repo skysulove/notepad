fb4fa61341ee4a66cfa0f3a1d09f6c2f21348821
Lex Gnatovsky <skysurfersky@gmail.com>
2020-07-14 01:37:40 +0300

    New version, with SQLite support

--- link.rb
+++ link.rb
@@ -1,6 +1,5 @@
-
+# Класс «Ссылка», разновидность базового класса «Запись»
 class Link < Post
-
   def initialize
     super
 
@@ -8,16 +7,35 @@ class Link < Post
   end
 
   def read_from_console
-    puts "Адрес ссылки:"
+    puts 'Адрес ссылки (url):'
     @url = STDIN.gets.chomp
 
-    puts "Что за ссылка?"
+    puts 'Что за ссылка?'
     @text = STDIN.gets.chomp
   end
 
   def to_strings
-    time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')} \n\r \n\r"
+    time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')} \n"
 
-    return [@url, @text, time_string]
+    [@url, @text, time_string]
+  end
+
+  # Метод to_db_hash у Задачи добавляет два ключа в хэш
+  def to_db_hash
+    # Вызываем родительский метод to_db_hash ключевым словом super. К хэшу,
+    # который он вернул добавляем специфичные для этого класса поля методом
+    # Hash#merge
+    super.merge('text' => @text, 'url' => @url)
+  end
+
+  # Метод load_data у Ссылки считывает дополнительно url ссылки
+  def load_data(data_hash)
+    # Сперва дергаем родительский метод load_data для общих полей. Обратите
+    # внимание, что вызов без параметров тут эквивалентен super(data_hash), так
+    # как те же параметры будут переданы методы super автоматически.
+    super
+
+    # Теперь достаем из хэша специфичное только для ссылки значение url
+    @url = data_hash['url']
   end
 end
--- main.rb
+++ main.rb
@@ -1,37 +1,36 @@
-if Gem.win_platform?
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
-
+# Подключаем класс Post и его детей: Memo, Link, Task
 require_relative 'post'
 require_relative 'memo'
 require_relative 'link'
 require_relative 'task'
 
+# Здороваемся с пользователем и спрашиваем у него, какую запись он хочет создать
 puts 'Привет, я твой блокнот!'
+puts 'Версия 2, записываю новые записи в базу SQLite'
 puts
 puts 'Что хотите записать в блокнот?'
 
-choices = Post.post_types
+# Выводим массив возможных типов Записи (поста) с помощью метода post_types
+# класса Post, который теперь возвращает не массив классов, а хэш.
+choices = Post.post_types.keys
 
 choice = -1
-
 until choice >= 0 && choice < choices.size
   choices.each_with_index do |type, index|
     puts "\t#{index}. #{type}"
   end
-
   choice = gets.to_i
 end
 
-entry = Post.create(choice)
+# Как только выбор сделан, мы можем создать запись нужного типа, передав выбор
+# строку с название класса в статический метод create класса Post.
+entry = Post.create(choices[choice])
 
 entry.read_from_console
 
-entry.save
+# Сохраняем пост в базу данных
+rowid = entry.save_to_db
 
-puts 'Ваша запись сохранена!'
+puts "Запись сохранена в базе, id = #{rowid}"
+#
+# Как обычно, при использовании классов программа выглядит очень лаконично!
